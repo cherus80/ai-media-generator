1f2e333aebbb0178ef59d8ca1b15b544dc26b64f
cherus80 <cherus09@gmail.com>
2025-11-28 19:23:07 +0700

    Shorten merge alembic revision id to fit column

--- backend/alembic/versions/20251128_1300_merge_fit_prompts_and_refresh_tokens.py
+++ backend/alembic/versions/20251128_1300_merge_fit_prompts_and_refresh_tokens.py
@@ -11,7 +11,7 @@ import sqlalchemy as sa  # noqa: F401
 
 
 # revision identifiers, used by Alembic.
-revision: str = "20251128_1300_merge_fit_prompts_and_refresh_tokens"
+revision: str = "20251128_merge_fit_refresh"
 down_revision: Union[str, None] = ("20251127_fit_prompts", "20251128_refresh_tokens")
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
